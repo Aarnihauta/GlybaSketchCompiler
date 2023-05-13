ba0b5dd6c5f84dc8cbb5df995655dcd30c1bb65a
Nikita <shinigamichan@yandex.ru>
2023-05-13 13:45:43 +0300

    Добавлены тесты на Parser

--- TokinizationTests/ParserTests.cs
+++ TokinizationTests/ParserTests.cs
@@ -1,4 +1,6 @@
-﻿using GlybaSketchCompiler.Parser;
+﻿using GlybaSketchCompiler.Expressions;
+using GlybaSketchCompiler.Parser;
+using GlybaSketchCompiler.Tokinization;
 
 namespace TokinizationTests;
 public class ParserTests
@@ -9,5 +11,29 @@ public class ParserTests
         var parser = new Parser("1*2-3");
         var syntax = parser.Parse();
         Assert.NotNull(syntax);
+    }
+
+    [Fact]
+    public void BinaryExpressionTest1()
+    {
+        var parser = new Parser("1-2");
+        var syntax = parser.Parse();
+
+        var children = syntax.GetChildren().ToList();
+
+        Assert.Equal(3, children.Count);
+        Assert.IsType<BinaryExpressionSyntax>(syntax);
+        Assert.IsType<NumberExpressionSyntax>(children[0]);
+        Assert.IsType<SyntaxToken>(children[1]);
+        Assert.IsType<NumberExpressionSyntax>(children[2]);
+    }
+
+    [Fact]
+    public void BinaryExpressionTest2()
+    {
+        var parser = new Parser("1-2+3");
+        var syntax = parser.Parse();
+
+        var children = syntax.GetChildren().ToList();
     }
 }
